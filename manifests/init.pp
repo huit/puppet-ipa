35333e74a35ea6dee9092c27e1cd674b9ca79c1b
robruma <robruma@gmail.com>
2013-08-24 15:12:36 -0400

    Updated dependency chain to include kstart

--- manifests/init.pp
+++ manifests/init.pp
@@ -144,7 +144,8 @@ class ipa (
   @cron { "k5start_root":
     command => "/usr/bin/k5start -f /etc/krb5.keytab -U -o root -k /tmp/krb5cc_0 > /dev/null 2>&1",
     user    => 'root',
-    minute  => "*/1"
+    minute  => "*/1",
+    require => Package["kstart"]
   }
 
   if $ipa::master and $ipa::replica {
