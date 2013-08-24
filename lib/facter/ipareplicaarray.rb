fa6174770d8601d044d57d0b82c7d2e255101124
robruma <robruma@gmail.com>
2013-08-24 14:28:02 -0400

    Updated ipareplicaarray fact

--- lib/facter/ipareplicaarray.rb
+++ lib/facter/ipareplicaarray.rb
@@ -3,13 +3,18 @@ Facter.add(:ipareplicaarray) do
     begin
       host = Facter.value(:hostname)
       domain = Facter.value(:domain)
-      if host and domain
-        fqdn = [host, domain].join(".")
+      ipaadminhomedir = Facter.value(:ipaadminhomedir)
+      if File.directory?(ipaadminhomedir)
+        if host and domain
+          fqdn = [host, domain].join(".")
+        else
+          nil
+        end
+        replicas = `/sbin/runuser -l admin -c '/usr/sbin/ipa-replica-manage list' 2>/dev/null | /bin/grep -v #{fqdn} | /bin/cut -d: -f1 | /usr/bin/tr -t '\n' ','`
+        replicas.chop.scan(/[\w.-]+/).map(&:to_s).to_a.combination(2).to_a.inspect
       else
         nil
       end
-      replicas = `/sbin/runuser -l admin -c '/usr/sbin/ipa-replica-manage list' 2>/dev/null | /bin/grep -v #{fqdn} | /bin/cut -d: -f1 | /usr/bin/tr -t '\n' ','`
-      replicas.chop.scan(/[\w.-]+/).map(&:to_s).to_a.combination(2).to_a.inspect
     end
   end
 end
