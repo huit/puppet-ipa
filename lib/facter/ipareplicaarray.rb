b44d136429f6324c3c845137d49ba486bcbfa464
robruma <robruma@gmail.com>
2013-08-26 16:36:57 -0400

    Updated ipareplicaarray fact, added replicas2array function, thanks Steve

--- lib/facter/ipareplicaarray.rb
+++ lib/facter/ipareplicaarray.rb
@@ -1,20 +1,17 @@
 Facter.add(:ipareplicaarray) do
-  setcode do
-    begin
-      host = Facter.value(:hostname)
-      domain = Facter.value(:domain)
-      ipaadminhomedir = Facter.value(:ipaadminhomedir)
-      if File.directory?(ipaadminhomedir)
-        if host and domain
-          fqdn = [host, domain].join(".")
-        else
-          nil
-        end
-        replicas = `/sbin/runuser -l admin -c '/usr/sbin/ipa-replica-manage list' 2>/dev/null | /bin/grep -v #{fqdn} | /bin/cut -d: -f1 | /usr/bin/tr -t '\n' ','`
-        replicas.chop.scan(/[\w.-]+/).map(&:to_s).to_a.combination(2).to_a.inspect
-      else
-        nil
-      end
-    end
-  end
+ setcode do
+   host = Facter.value(:hostname)
+   domain = Facter.value(:domain)
+   ipaadminhomedir = Facter.value(:ipaadminhomedir)
+   if File.directory?(ipaadminhomedir)
+     if host and domain
+       fqdn = [host, domain].join(".")
+     end
+   elsif File.directory?(ipaadminhomedir).nil?
+     'UNKNOWN'
+   end
+   servers = Facter::Util::Resolution.exec("/sbin/runuser -l admin -c '/usr/sbin/ipa-replica-manage list' 2>/dev/null | /bin/grep -v #{fqdn} | /bin/cut -d: -f1")
+   combinations = servers.scan(/[\w.-]+/).combination(2).to_a
+   combinations.collect { |combination| combination.join(',') }.join(':')
+ end
 end
