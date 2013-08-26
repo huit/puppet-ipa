90ad8312e960ff864121e03f7277345537cd2100
robruma <robruma@gmail.com>
2013-08-26 16:57:58 -0400

    Updated replias2array function

--- lib/puppet/parser/functions/replicas2array.rb
+++ lib/puppet/parser/functions/replicas2array.rb
@@ -1,12 +1,13 @@
 module Puppet::Parser::Functions
-newfunction(:replicas2array, :type => :rvalue, :doc => '<docstring>') do |arguments|
- raise(Puppet::ParseError, "suffix(): Wrong number of arguments " + "given (#{arguments.size} for 1)")
- if arguments.size != 1
-   packed = arguments[0]
-   unless packed.is_a?(String)
-     raise(Puppet::ParseError, 'replicas2array(): Requires string to work with')
-   end
-   unpacked = packed.split(':').collect {|pair| pair.split(',')}
-   return unpacked
- end
+  newfunction(:replicas2array, :type => :rvalue, :doc => '<docstring>') do |arguments|
+    raise(Puppet::ParseError, "suffix(): Wrong number of arguments " + "given (#{arguments.size} for 1)")
+    if arguments.size != 1
+      packed = arguments[0]
+        unless packed.is_a?(String)
+          raise(Puppet::ParseError, 'replicas2array(): Requires string to work with')
+        end
+      unpacked = packed.split(':').collect {|pair| pair.split(',')}
+      return unpacked
+    end
+  end
 end
