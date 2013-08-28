cd2aa9ad02b762c882a4cfec76951cf545c461a6
robruma <robruma@gmail.com>
2013-08-28 14:44:48 -0400

    Corrected condition logic

--- lib/puppet/parser/functions/replicas2array.rb
+++ lib/puppet/parser/functions/replicas2array.rb
@@ -1,7 +1,8 @@
 module Puppet::Parser::Functions
   newfunction(:replicas2array, :type => :rvalue, :doc => '<docstring>') do |arguments|
-    raise(Puppet::ParseError, "suffix(): Wrong number of arguments " + "given (#{arguments.size} for 1)")
     if arguments.size != 1
+      raise(Puppet::ParseError, "suffix(): Wrong number of arguments " + "given (#{arguments.size} for 1)")
+    else
       packed = arguments[0]
         unless packed.is_a?(String)
           raise(Puppet::ParseError, 'replicas2array(): Requires string to work with')
