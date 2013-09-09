aa304b78e151c770537ac8af4d001ab4ea0246e8
Steve Huff <shuff@vecna.org>
2013-09-09 12:31:15 -0400

    Rewriting ipa::replicaagreement
    Requires a unique $title, takes $from and $to params

--- manifests/replicaagreement.pp
+++ manifests/replicaagreement.pp
@@ -1,9 +1,9 @@
-define ipa::replicaagreement () {
+define ipa::replicaagreement (
+  $from,
+  $to
+) {
 
-  $from = $name[0]
-  $to   = $name[1]
-
-  exec { "connectreplicas-${host}":
+  exec { "connectreplicas-${from}-${to}":
     command     => "/sbin/runuser -l admin -c \'/usr/sbin/ipa-replica-manage connect --cacert=/etc/ipa/ca.crt ${from} ${to}\'",
     unless      => "/sbin/runuser -l admin -c \'/usr/sbin/ipa-replica-manage list ${from} | /bin/grep ${to} >/dev/null 2>&1\'",
     logoutput   => "on_failure"
