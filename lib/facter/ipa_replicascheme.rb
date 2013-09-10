10e92bedf9fa2b094b9efbe32e6756914e9116ba
robruma <robruma@gmail.com>
2013-09-10 19:34:39 -0400

    Excluding winsync replicas from ipa_replicascheme

--- lib/facter/ipa_replicascheme.rb
+++ lib/facter/ipa_replicascheme.rb
@@ -10,7 +10,7 @@ Facter.add(:ipa_replicascheme) do
    elsif File.directory?(ipaadminhomedir).nil?
      'UNKNOWN'
    end
-   servers = Facter::Util::Resolution.exec("/sbin/runuser -l admin -c '/usr/sbin/ipa-replica-manage list' 2>/dev/null | /bin/grep -v #{fqdn} | /bin/cut -d: -f1")
+   servers = Facter::Util::Resolution.exec("/sbin/runuser -l admin -c '/usr/sbin/ipa-replica-manage list' 2>/dev/null | /bin/egrep -v '#{fqdn}|winsync' | /bin/cut -d: -f1")
    combinations = servers.scan(/[\w.-]+/).combination(2).to_a
    combinations.collect { |combination| combination.join(',') }.join(':')
  end
