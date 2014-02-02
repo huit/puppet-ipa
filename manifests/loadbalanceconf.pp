22d40429f3d6f10e13bcecb06d2726c3e9763db0
robruma <robruma@gmail.com>
2014-02-02 11:24:25 -0500

    Removing another notify

--- manifests/loadbalanceconf.pp
+++ manifests/loadbalanceconf.pp
@@ -1,3 +1,6 @@
+# Definition: ipa::loadbalanceconf
+#
+# Configures IPA load balancing
 define ipa::loadbalanceconf (
   $host       = $name,
   $domain     = {},
@@ -11,7 +14,7 @@ define ipa::loadbalanceconf (
 
   case $::osfamily {
     'Debian': {
-      notify { "Unable to configure load balanced IPA directory services for Debian.": }
+      notify { 'Unable to configure load balanced IPA directory services for Debian.': }
     }
     default: {
       $mkhomediropt = $mkhomedir ? {
@@ -21,8 +24,8 @@ define ipa::loadbalanceconf (
 
       exec { "loadbalanceconf-authconfig-${host}":
         command     => "/usr/sbin/authconfig --nostart --enablesssd --enableldap --ldapserver=${servers} --ldapbasedn=${dc} --krb5kdc=${servers} --krb5adminserver=${servers} ${mkhomediropt} --update",
-        logoutput   => "on_failure"
-      }<- notify { "Configuring load balanced IPA directory services, please wait.": } ~> Ipa::Flushcache["loadbalanceconf-flushcache-${host}"]
+        logoutput   => 'on_failure'
+      } ~> Ipa::Flushcache["loadbalanceconf-flushcache-${host}"]
     }
   }
 
