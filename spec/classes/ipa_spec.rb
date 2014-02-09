e1b784e1f256a234d49f913f6f54c968e3343091
robruma <robruma@gmail.com>
2014-02-09 09:02:21 -0500

    Updating rspec tests

--- spec/classes/ipa_spec.rb
+++ spec/classes/ipa_spec.rb
@@ -4,8 +4,9 @@ describe 'ipa', :type => :class do
 
   context 'with master => true' do
     describe "ipa::init" do
-      let(:params) { { :master => true  } }
-      it { should create_class('ipa::master')}
+      let(:params) { { :master => true, :cleanup => false  } }
+      it { should create_class('ipa::master') }
+      it { should contain_package('ipa') }
     end
   end
 end
