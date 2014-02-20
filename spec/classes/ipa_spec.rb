6824f6fa9c2b9b3f061505d4778d9337fe1d97fc
robruma <robruma@gmail.com>
2014-02-20 12:00:15 -0500

    Adding the begining of spec tests

--- spec/classes/ipa_spec.rb
+++ spec/classes/ipa_spec.rb
@@ -4,8 +4,17 @@ describe 'ipa', :type => :class do
 
   context 'with master => true' do
     describe "ipa::init" do
-      let(:params) { { :master => true, :cleanup => false  } }
-      it { should create_class('ipa::master') }
+      let(:params) {
+        {
+          :master  => true,
+          :cleanup => false,
+          :adminpw => '12345678',
+          :dspw    => '12345678',
+          :domain  => 'test.domain.org',
+          :realm   => 'TEST.DOMAIN.ORG'
+        }
+      }
+      it { should contain_class('ipa::master') }
       it { should contain_package('ipa') }
     end
   end
