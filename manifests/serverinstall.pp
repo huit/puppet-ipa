03d6bd4780b3008391e1a15d3356280e2ac52c10
Jordan Wilson <jordan@bitbrew.com>
2016-11-08 12:49:22 -0500

    .

--- manifests/serverinstall.pp
+++ manifests/serverinstall.pp
@@ -23,26 +23,23 @@ define ipa::serverinstall (
     mode    =>  644,
   }
 
-  if $::restore == "true" {
-    exec { "download backup":
+
+
+  if ($::restore == "true") {
+    $install_command = shellquote("ipa-restore /var/lib/ipa/backup/${restore_dir} --unattended --password ${adminpw}")
+    exec { 'download s3 backup':
       command => "aws s3 cp s3://management-hub-${region}-s3-credentials/ipa_backups/${restore_dir}/ /var/lib/ipa/backup/latest/ --recursive"
-    } ->
-    exec { "restoring from s3 backup":
-      command   => "ipa-restore /var/lib/ipa/backup/${restore_dir} --unattended --password ${adminpw}",
-      creates   => '/etc/ipa/default.conf',
-      notify    => Ipa::Flushcache["server-${host}"]
     }
+  } else {
+      $install_command = shellquote("/usr/sbin/ipa-server-install --hostname=${host} --realm=${realm} --domain=${domain} --admin-password='${adminpw}' --ds-password='${dspw}' ${dnsopt} ${forwarderopts} ${ntpopt} ${extcaopt} ${idstartopt} --unattended")
+  }
 
-
-  } elsif $::restore == "false" {
-      exec { "serverinstall-${host}":
-        command   => "/usr/sbin/ipa-server-install --hostname=${host} --realm=${realm} --domain=${domain} --admin-password='${adminpw}' --ds-password='${dspw}' ${dnsopt} ${forwarderopts} ${ntpopt} ${extcaopt} ${idstartopt} --unattended",
-        timeout   => '0',
-        unless    => '/usr/sbin/ipactl status >/dev/null 2>&1',
-        creates   => '/etc/ipa/default.conf',
-        notify    => Ipa::Flushcache["server-${host}"],
-        logoutput => 'on_failure'
-      }
+  exec { "serverinstall-${host}":
+    command   => "${install_command}",
+    timeout   => '0',
+    unless    => '/usr/sbin/ipactl status >/dev/null 2>&1',
+    creates   => '/etc/ipa/default.conf',
+    notify    => Ipa::Flushcache["server-${host}"]
   }
 
   ipa::flushcache { "server-${host}":
