ddba4ac6bec226db5c4e869935f789f863fb942a
Jordan Wilson <jordan@bitbrew.com>
2016-11-08 11:41:56 -0500

    .

--- manifests/serverinstall.pp
+++ manifests/serverinstall.pp
@@ -11,20 +11,33 @@ define ipa::serverinstall (
   $forwarderopts = {},
   $ntpopt        = {},
   $extcaopt      = {},
-  $idstart       = {}
+  $idstart       = {},
 ) {
 
   $idstartopt = "--idstart=${idstart}"
 
   anchor { 'ipa::serverinstall::start': }
 
-  exec { "serverinstall-${host}":
-    command   => "/usr/sbin/ipa-server-install --hostname=${host} --realm=${realm} --domain=${domain} --admin-password='${adminpw}' --ds-password='${dspw}' ${dnsopt} ${forwarderopts} ${ntpopt} ${extcaopt} ${idstartopt} --unattended",
-    timeout   => '0',
-    unless    => '/usr/sbin/ipactl status >/dev/null 2>&1',
-    creates   => '/etc/ipa/default.conf',
-    notify    => Ipa::Flushcache["server-${host}"],
-    logoutput => 'on_failure'
+  if $::restore == "true" {
+    exec { "download backup":
+      command => "aws s3 cp s3://management-hub-${region}-s3-credentials/ipa_backups/${restore_dir}/ /var/lib/ipa/backup/latest/ --recursive"
+    } ->
+    exec { "restoring from s3 backup":
+      command   => "ipa-restore /var/lib/ipa/backup/${restore_dir} --unattended --password ${adminpw}",
+      creates   => '/etc/ipa/default.conf',
+      notify    => Ipa::Flushcache["server-${host}"]
+    }
+
+
+  } elsif $::restore == "false" {
+      exec { "serverinstall-${host}":
+        command   => "/usr/sbin/ipa-server-install --hostname=${host} --realm=${realm} --domain=${domain} --admin-password='${adminpw}' --ds-password='${dspw}' ${dnsopt} ${forwarderopts} ${ntpopt} ${extcaopt} ${idstartopt} --unattended",
+        timeout   => '0',
+        unless    => '/usr/sbin/ipactl status >/dev/null 2>&1',
+        creates   => '/etc/ipa/default.conf',
+        notify    => Ipa::Flushcache["server-${host}"],
+        logoutput => 'on_failure'
+      }
   }
 
   ipa::flushcache { "server-${host}":
@@ -51,21 +64,6 @@ define ipa::serverinstall (
   exec { 'authorize-home-dirs':
     command => 'authconfig --enablemkhomedir --update',
     require => Anchor['ipa::serverinstall::end']
-  }
-
-  if $::restore == "true" {
-    exec { "download backup":
-      command => "aws s3 cp s3://management-hub-${region}-s3-credentials/ipa_backups/${restore_dir} /var/lib/ipa/backup/ --recursive",
-      require => Anchor['ipa::serverinstall::end']
-    } ->
-    exec { "restoring from s3 backup":
-      command   => "ipa-restore --password ${admpw} /var/lib/ipa/backup"
-    } ->
-    ::ipa::replicaprepare { 'replicaprepare':
-      adminpw         => $adminpw,
-      dspw            => $dspw,
-      require         => Anchor['ipa::serverinstall::end']
-    }
   }
 
 }
