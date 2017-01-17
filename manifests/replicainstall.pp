b04f98711f7bc04e8e54866715eb2ffc2113bf03
Jordan Wilson <jordan@bitbrew.com>
2017-01-17 14:18:20 -0500

    change domain name

--- manifests/replicainstall.pp
+++ manifests/replicainstall.pp
@@ -9,12 +9,12 @@ define ipa::replicainstall (
 ) {
 
   exec { "add master host entry":
-    command => "echo $(dig +short freeipa-master-${::environment}.${domain})  freeipa-master-${::environment}.${domain} freeipa-master-${::environment} >> /etc/hosts",
+    command => "echo $(dig +short ${::freeipa_hostname}-master-${::environment}.${domain})  ${::freeipa_hostname}-master-${::environment}.${domain} ${::freeipa_hostname}-master-${::environment} >> /etc/hosts",
     before  => Exec["replicainstall-${host}"],
   }
 
   exec { "replicainstall-${host}":
-    command     => "/usr/sbin/ipa-replica-install --hostname=${::ec2_public_hostname} --skip-conncheck --setup-ca --principal=admin --admin-password=${adminpw} --server=freeipa-master-${::environment}.infra.bitbrew.com --domain=infra.bitbrew.com --realm=INFRA.BITBREW.COM --unattended --no-host-dns --mkhomedir",
+    command     => "/usr/sbin/ipa-replica-install --hostname=${::ec2_public_hostname} --skip-conncheck --setup-ca --principal=admin --admin-password=${adminpw} --server=${::freeipa_hostname}-master-${::environment}.infra.bitbrew.com --domain=infra.bitbrew.com --realm=INFRA.BITBREW.COM --unattended --no-host-dns --mkhomedir",
     timeout     => '0',
     onlyif      => 'test ! -f /etc/ipa/ca.crt',
   }
